84c84889c501dcb2d1527ad2e60475770bff1426
Gennadiy Turov <mirage216@mail.ru>
2020-10-18 12:04:48 +0400

    2020-10-18 morning

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <numeric>
 #include <cassert>
+#include <queue>
 
 
 using namespace std;
@@ -940,24 +941,72 @@ void MatchDocuments(const SearchServer& search_server, const string& query) {
     }
 }
 
-int main() {
-    //TestSearchServer();
-    SearchServer search_server("and with"s);
-
-    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
-    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2, 3});
-    search_server.AddDocument(3, "big cat nasty hair"s, DocumentStatus::ACTUAL, {1, 2, 8});
-    search_server.AddDocument(4, "big dog cat Vladislav"s, DocumentStatus::ACTUAL, {1, 3, 2});
-    search_server.AddDocument(5, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
-
-    const auto search_results = search_server.FindTopDocuments("curly dog"s);
-    int page_size = 2;
-    const auto pages = Paginate(search_results, page_size);
-
-    // Выводим найденные документы по страницам
-    for (auto page = pages.begin(); page != pages.end(); ++page) {
-        cout << *page << endl;
-        cout << "Page break"s << endl;
-    }
+class RequestQueue {
+public:
+explicit RequestQueue(const SearchServer& search_server):
+        search_server_(search_server)    {
 }
 
+template <typename DocumentPredicate>
+vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+    return ProcessQeque(raw_query,search_server_.FindTopDocuments(raw_query,document_predicate));
+}
+
+vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status) {
+    return ProcessQeque(raw_query,search_server_.FindTopDocuments(raw_query, status));
+}
+
+vector<Document> AddFindRequest(const string& raw_query) {
+    return ProcessQeque(raw_query,search_server_.FindTopDocuments(raw_query));
+}
+
+int GetNoResultRequests() const {
+    return count_if(requests_.begin(),requests_.end(),[](const QueryResult& r){return r.isNoResult;});
+}
+private:
+struct QueryResult {
+    string raw_query;
+    vector<Document> result;
+    bool isNoResult;
+};
+
+vector<Document> ProcessQeque(const string& raw_query, vector<Document> result) {
+    QueryResult r;
+    r.raw_query = raw_query;
+    r.result = result;
+    r.isNoResult = r.result.empty();
+    requests_.push_back(r);
+    if (requests_.size() > sec_in_day_) {
+        requests_.pop_front();
+    }
+    return r.result;
+}
+deque<QueryResult> requests_;
+const static int sec_in_day_ = 1440;
+const SearchServer& search_server_;
+};
+
+int main() {
+    TestSearchServer();
+    SearchServer search_server("и в на"s);
+    RequestQueue request_queue(search_server);
+
+    search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "большой кот модный ошейник "s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "большой пёс скворец евгений"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "большой пёс скворец василий"s, DocumentStatus::ACTUAL, {1, 1, 1});
+
+    // 1439 запросов с нулевым результатом
+    for (int i = 0; i < 1439; ++i) {
+        request_queue.AddFindRequest("empty query"s);
+    }
+    // все еще 1439 запросов с нулевым результатом
+    request_queue.AddFindRequest("пушистый пёс"s);
+    // новые сутки, первый запрос удален, 1438 запросов с нулевым результатом
+    request_queue.AddFindRequest("большой ошейник"s);
+    // первый запрос удален, 1437 запросов с нулевым результатом
+    request_queue.AddFindRequest("скворец"s);
+    cout << "Запросов, по которым ничего не нашлось "s << request_queue.GetNoResultRequests();
+    return 0;
+}
